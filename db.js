425b71604ff9c7074cbb89f296e3c1b0923bb35d
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-12-06 15:37:04 +0530

    Host

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool
 const pool = new Pool({
     user:"postgres",
     password:"123123",
-    host:"postgres-service",
+    host:"192.168.0.105",
     port:31002,
     database:"todo"
 })
