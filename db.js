4dc361057d7103142bd4082a1b9973501bf1996d
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-12-06 13:08:44 +0530

    Modify db host name

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool
 const pool = new Pool({
     user:"postgres",
     password:"123123",
-    host:"postgres",
+    host:"postgres-service",
     port:5433,
     database:"todo"
 })
