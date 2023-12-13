c21ffc1d7604ee30fc78058d04f8cddead9b8e83
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-12-13 12:09:36 +0530

    modify url to master node (RASPBERRY PI)

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool
 const pool = new Pool({
     user:"postgres",
     password:"123123",
-    host:"192.168.0.105",
+    host:"192.168.100.200",
     port:31002,
     database:"todo"
 })
